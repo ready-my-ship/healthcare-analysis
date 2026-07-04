f1e12bfff9dc372461832216f09e2167b1ade40b
D <attribution.matters@gmail.com>
2026-07-04 16:32:36 -0400

    silver transformations completed

--- 04_silver.sql
+++ 04_silver.sql
@@ -33,25 +33,117 @@ SELECT
     PHONE_NUMBER,
     MEASURE_ID,
     QUESTION,
-    CASE 
-        WHEN QUESTION IN (
-            'Patients who reported that their nurses "Always" communicated well',
-            'Patients who reported that their nurses "Usually" communicated well',
-            'Patients who reported that their nurses "Sometimes" or "Never" communicated well'
+    CASE
+
+    -- Nurse Communication
+    WHEN QUESTION IN (
+        'Patients who reported that their nurses "Always" communicated well',
+        'Patients who reported that their nurses "Usually" communicated well',
+        'Patients who reported that their nurses "Sometimes" or "Never" communicated well'
     )
     THEN 'NURSE_COMMUNICATION'
-        WHEN QUESTION IN (
-            'Patients who reported that their doctors "Always" communicated well',
-            'Patients who reported that their doctors "Usually" communicated well',
-            'Patients who reported that their doctors "Sometimes" or "Never" communicated well'
+
+    -- Doctor Communication
+    WHEN QUESTION IN (
+        'Patients who reported that their doctors "Always" communicated well',
+        'Patients who reported that their doctors "Usually" communicated well',
+        'Patients who reported that their doctors "Sometimes" or "Never" communicated well'
     )
     THEN 'DOCTOR_COMMUNICATION'
-        WHEN QUESTION IN (
-            'Patients who reported that their room and bathroom were "Always" clean',
-            'Patients who reported that their room and bathroom were "Usually" clean',
-            'Patients who reported that their room and bathroom were "Sometimes" or "Never" clean'
+
+    -- Staff Responsiveness
+    WHEN QUESTION IN (
+        'Patients who reported that they "Always" received help as soon as they wanted',
+        'Patients who reported that they "Usually" received help as soon as they wanted',
+        'Patients who reported that they "Sometimes" or "Never" received help as soon as they wanted'
     )
-    THEN 'CLEANLINESS'
+    THEN 'STAFF_RESPONSIVENESS'
+
+    -- Room & Bathroom Cleanliness
+    WHEN QUESTION IN (
+        'Patients who reported that their room and bathroom were "Always" clean',
+        'Patients who reported that their room and bathroom were "Usually" clean',
+        'Patients who reported that their room and bathroom were "Sometimes" or "Never" clean'
+    )
+    THEN 'ROOM_AND_BATHROOM_CLEANLINESS'
+
+    -- Quietness
+    WHEN QUESTION IN (
+        'Patients who reported that the area around their room was "Always" quiet at night',
+        'Patients who reported that the area around their room was "Usually" quiet at night',
+        'Patients who reported that the area around their room was "Sometimes" or "Never" quiet at night'
+    )
+    THEN 'QUIETNESS'
+
+    -- Medication Communication
+    WHEN QUESTION IN (
+        'Patients who reported that staff "Always" explained about medicines before giving it to them',
+        'Patients who reported that staff "Usually" explained about medicines before giving it to them',
+        'Patients who reported that staff "Sometimes" or "Never" explained about medicines before giving it to them'
+    )
+    THEN 'MEDICATION_COMMUNICATION'
+
+    -- Home Recovery Information
+    WHEN QUESTION IN (
+        'Patients who reported that YES, they were given information about what to do during their recovery at home',
+        'Patients who reported that NO, they were not given information about what to do during their recovery at home'
+    )
+    THEN 'HOME_RECOVERY_INFORMATION'
+
+    -- Understood Care
+    WHEN QUESTION IN (
+        'Patients who "Strongly Agree" they understood their care when they left the hospital',
+        'Patients who "Agree" they understood their care when they left the hospital',
+        'Patients who "Disagree" or "Strongly Disagree" they understood their care when they left the hospital'
+    )
+    THEN 'UNDERSTOOD_CARE'
+
+    -- Recommend Hospital
+    WHEN QUESTION IN (
+        'Patients who reported YES, they would definitely recommend the hospital',
+        'Patients who reported YES, they would probably recommend the hospital',
+        'Patients who reported NO, they would probably not or definitely not recommend the hospital'
+    )
+    THEN 'RECOMMEND_HOSPITAL'
+
+    -- Overall Hospital Rating
+    WHEN QUESTION IN (
+        'Patients who gave their hospital a rating of 9 or 10 on a scale from 0 (lowest) to 10 (highest)',
+        'Patients who gave their hospital a rating of 7 or 8 on a scale from 0 (lowest) to 10 (highest)',
+        'Patients who gave their hospital a rating of 6 or lower on a scale from 0 (lowest) to 10 (highest)'
+    )
+    THEN 'HOSPITAL_RATING'
+
+    -- Star Ratings
+    WHEN QUESTION IN (
+        'Nurse communication - star rating',
+        'Doctor communication - star rating',
+        'Staff responsiveness - star rating',
+        'Quietness - star rating',
+        'Communication about medicines - star rating',
+        'Cleanliness - star rating',
+        'Discharge information - star rating',
+        'Care transition - star rating',
+        'Recommend hospital - star rating',
+        'Overall hospital rating - star rating',
+        'Summary star rating'
+    )
+    THEN 'STAR_RATING'
+
+    -- Linear Mean Scores
+    WHEN QUESTION IN (
+        'Nurse communication - linear mean score',
+        'Doctor communication - linear mean score',
+        'Staff responsiveness - linear mean score',
+        'Quietness - linear mean score',
+        'Communication about medicines - linear mean score',
+        'Cleanliness - linear mean score',
+        'Discharge information - linear mean score',
+        'Care transition - linear mean score',
+        'Overall hospital rating - linear mean score',
+        'Recommend hospital - linear mean score'
+    )
+    THEN 'LINEAR_MEAN_SCORE'
     ELSE NULL
     END AS QUESTION_CATEGORY,
     ANSWER_DESCRIPTION,
@@ -69,21 +161,42 @@ SELECT
     LOCATION
 FROM BRONZE.SURVEY;
 
+-- see if table is there
+SHOW TABLES;
+
+--VERIFY SCHEMA (COLUMNS AND DATATYPES)
+DESC TABLE SILVER.SURVEY
+
+-- shows if CASE worked
+SELECT DISTINCT QUESTION_CATEGORY
+FROM SURVEY
+ORDER BY QUESTION_CATEGORY;
+
+-- verify
+SELECT *
+FROM SILVER.SURVEY
+LIMIT 10;
+
+SELECT DISTINCT QUESTION
+FROM BRONZE.SURVEY
+WHERE QUESTION LIKE '%clean%';
+-- see all things null
+SELECT DISTINCT 
+    QUESTION,
+    QUESTION_CATEGORY,
+FROM SILVER.SURVEY
+WHERE QUESTION_CATEGORY IS NULL;
 
 
+SELECT QUESTION
+FROM BRONZE.SURVEY
+WHERE QUESTION = 'Summary star rating'
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT DISTINCT
+    QUESTION,
+    QUESTION_CATEGORY
+FROM SILVER.SURVEY
+WHERE QUESTION_CATEGORY IS NULL;
 
 
